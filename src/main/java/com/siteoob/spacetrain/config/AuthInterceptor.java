68b2284970592d3bfbb59899effbf1bcdce10007
Matheus Santos Casado <matheusjotinha2@gmail.com>
2026-06-15 15:45:29 -0300

    Atualização auth interceptor

--- src/main/java/com/siteoob/spacetrain/config/AuthInterceptor.java
+++ src/main/java/com/siteoob/spacetrain/config/AuthInterceptor.java
@@ -13,9 +13,6 @@ public class AuthInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String uri = request.getRequestURI();
-        if (uri.equals("/login") || uri.equals("/cadastrar") || uri.equals("/logout") || uri.startsWith("/css") || uri.startsWith("/js") || uri.startsWith("/images") || uri.equals("/error") || uri.equals("/favicon.ico") || uri.startsWith("/uploads")) {
-            return true;
-        }
 
         HttpSession session = request.getSession(false);
         boolean loggedIn = session != null && session.getAttribute("usuarioId") != null;
